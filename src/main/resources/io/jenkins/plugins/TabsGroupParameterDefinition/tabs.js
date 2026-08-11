97b010470747a4a64724fc8fa47b2733ca97cd70
YanekC <yanek.colonge@systeme-u.fr>
2026-08-11 09:22:25 +0200

    fix: modernize javascript

--- src/main/resources/io/jenkins/plugins/TabsGroupParameterDefinition/tabs.js
+++ src/main/resources/io/jenkins/plugins/TabsGroupParameterDefinition/tabs.js
@@ -3,21 +3,23 @@ function openTab(tabButton) {
     let i, tabcontent, tablinks;
 
     // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+    tabcontent = document.querySelectorAll(".tabcontent");
+    tabcontent.forEach(tab => {
+        //hide tab content
+        tab.style.display = "none";
+    })
 
     // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+    tablinks = document.querySelectorAll(".tablinks");
+    tablinks.forEach(tab => {
+        //reset active class
+        tab.classList.remove("active");
+    })
 
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabButton.dataset.tabName).style.display = "block";
     document.getElementById("selected-tab-input").value = tabButton.innerText;
-    tabButton.className += " active";
+    tabButton.classList.add("active");
 }
 
 function docReady(fn) {
