0b4b83ffd77aa87dd54b64ab5f051c8f111bc876
YanekC <yanek.colonge@systeme-u.fr>
2026-09-02 09:12:57 +0200

    feat: use default Jenkins css

--- src/main/resources/io/jenkins/plugins/TabsGroupParameterDefinition/tabs.js
+++ src/main/resources/io/jenkins/plugins/TabsGroupParameterDefinition/tabs.js
@@ -5,21 +5,20 @@ function openTab(tabButton) {
     // Get all elements with class="tabcontent" and hide them
     tabcontent = document.querySelectorAll(".tabcontent");
     tabcontent.forEach(tab => {
-        //hide tab content
-        tab.style.display = "none";
+        tab.classList.add("jenkins-hidden");
     })
 
     // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.querySelectorAll(".tablinks");
+    tablinks = document.querySelectorAll(".tab");
     tablinks.forEach(tab => {
         //reset active class
         tab.classList.remove("active");
     })
 
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabButton.dataset.tabUid).style.display = "block";
+    document.getElementById(tabButton.dataset.tabUid).classList.remove("jenkins-hidden");
     document.getElementById("selected-tab-input").value = tabButton.dataset.tabUid;
-    tabButton.classList.add("active");
+    tabButton.parentElement.classList.add("active");
 }
 
 function docReady(fn) {
@@ -38,10 +37,5 @@ docReady(function () {
         tabButtons[i].addEventListener("click", function (event) {
             openTab(event.currentTarget);
         });
-    }
-
-    const input = document.getElementsByClassName("active");
-    if (input[0] != null) {
-        openTab(input[0]);
     }
 });
